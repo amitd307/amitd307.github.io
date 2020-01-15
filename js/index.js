ff31ff36fa69895595a39a90f48b8121bff1d9a6
Smit Debnath <thunder11h@gmail.com>
2020-01-16 00:55:11 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -265,7 +265,7 @@ function onMouseDown(event) {
 
                           info.innerHTML = "I'm Amit <span>Debnath</span>,";
                         subtitle.innerHTML = "a <span>Developer</span> from <span>Agartala (India)<span>. I love turning exciting ideas into beautiful products.";
-                        description.innerHTML = "KCC Institute of Technology & Management, Grater Noida Class of <span>2021</span><br/><a target='_blank' href='https://github.com/sumitd913/sumitd913.github.io/raw/master/Sumit_DEBNATH_Resume.pdf'>Offline Resume</a><br/><br/><div>Click on the other planets to learn more ...<div>";
+                        description.innerHTML = "KCC Institute of Technology & Management, Grater Noida Class of <span>2021</span><br/><a target='_blank' href='https://github.com/amitd307/amitd307.github.io/raw/master/final-resume.pdf'>Offline Resume</a><br/><br/><div>Click on the other planets to learn more ...<div>";
 
                     }
                 }
